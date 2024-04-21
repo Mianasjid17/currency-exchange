b02dc59e393112e7a462a9c7503eff5c7f9d6b5c
Asjid Saeed <159787271+Mianasjid17@users.noreply.github.com>
2024-04-21 21:31:52 +0500

    Add files via upload

---
+++ currency.ts
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk  from 'chalk';
+
+
+const currency: any = {
+    USD: 1,
+    EURO: 0.92,
+    POUND: 0.79,
+    INR: 74.57,
+    PKR: 280,
+    KWD:0.30,
+    AED:3.67,
+    YEN:7,
+}
+
+async function convertCurrency() {
+    let userAnswer = await inquirer.prompt([{
+        name: "from",
+        type: "list",
+        message:"From which currency do you want to convert?",
+        choices: Object.keys(currency)
+    },
+    {
+        name: "to",
+        type: "list",
+        message:"To which currency do you want to convert?",
+        choices: Object.keys(currency)
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "How much currency do you want to convert?",
+    }]);
+
+    let result = calculateConversion(userAnswer.from, userAnswer.to, userAnswer.amount);
+
+    console.log(result);
+}
+function calculateConversion(from: string, to: string, amount: number) {
+    let fromAmount = currency[from];
+    let toAmount = currency[to];
+    let result;
+
+    switch (from) {
+        case "USD":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "EURO":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "POUND":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "INR":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "PKR":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "PKR":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "KWD":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "YEN":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "AED":
+            result = amount / fromAmount * toAmount;
+            break;    
+
+        default:
+            result = "Invalid currency";
+    }
+
+    return result;
+}
+
+convertCurrency();
